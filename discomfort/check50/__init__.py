ce1f1970d3d0520ba081b0772862c61e2164f432
Margaret Nest <>
2017-09-10 12:30:03 +0000

    modify wordcheck

--- discomfort/check50/__init__.py
+++ discomfort/check50/__init__.py
@@ -9,9 +9,9 @@ class Discomfort(Checks):
         pass
 
     @check()
-    def at_least_50_words(self):
-        """Assignment has at least 50 words"""
+    def at_least_200_words(self):
+        """Assignment has approx 300 to 500 words"""
         with open("discomfort.txt") as f:
             word_count = len(f.read().split())
-            if word_count < 50:
-                raise Error("50 words required, only {} found.".format(word_count))
+            if word_count < 200:
+                raise Error("300 to 500 words required, only {} found.".format(word_count))
