5ad13c415bd0d278b96f7d0b1fdcd3b0ede8232d
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2017-10-27 12:10:11 -0400

    Update __init__.py

--- isbnreflect/check50/__init__.py
+++ isbnreflect/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Discomfort(Checks):
+class Isbnreflect(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'What is my discomfort'"""
+        """You submitted 'ISBN Reflection'"""
         files = os.listdir()
-        if not any(filename.startswith("discomfort") for filename in files):
+        if not any(filename.startswith("isbnreflect") for filename in files):
             raise Error("File not found")
