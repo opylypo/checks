7a47fe5e9a715f83c4031e329f51b02f0c067171
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2017-11-02 12:46:52 -0400

    Update __init__.py

--- scratch_two/check50/__init__.py
+++ scratch_two/check50/__init__.py
@@ -4,7 +4,7 @@ class Techexplore(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Secone Scratch Project'"""
+        """You submitted 'Second Scratch Project'"""
         files = os.listdir()
         if not any(filename.startswith("scratch") for filename in files):
             raise Error("File not found")
