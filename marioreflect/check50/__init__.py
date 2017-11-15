a44d10e0b68afef463777d839436ec9e73e22939
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2017-11-15 09:46:11 -0500

    Update __init__.py

--- marioreflect/check50/__init__.py
+++ marioreflect/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Discomfort(Checks):
+class Marioreflect(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'What is my discomfort'"""
+        """You submitted 'Mario Reflection'"""
         files = os.listdir()
-        if not any(filename.startswith("discomfort") for filename in files):
+        if not any(filename.startswith("marioreflect") for filename in files):
             raise Error("File not found")
