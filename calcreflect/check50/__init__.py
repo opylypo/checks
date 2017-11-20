34f018fab76bf726d72808ce715797a3990f1afb
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2017-11-20 12:09:44 -0500

    Update __init__.py

--- calcreflect/check50/__init__.py
+++ calcreflect/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Greedyreflect(Checks):
+class Calcreflect(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Greedy Reflection'"""
+        """You submitted 'Calc Reflection'"""
         files = os.listdir()
-        if not any(filename.startswith("greedyreflect") for filename in files):
+        if not any(filename.startswith("calcreflect") for filename in files):
             raise Error("File not found")
