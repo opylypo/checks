12582bf66782f0c2c479f92b5da238f631faf0fe
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2017-11-28 09:06:49 -0500

    Update __init__.py

--- caesarreflect/check50/__init__.py
+++ caesarreflect/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Isbnreflect(Checks):
+class Caesarreflect(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'ISBN Reflection'"""
+        """You submitted 'Caesar Reflection'"""
         files = os.listdir()
-        if not any(filename.startswith("isbnreflect") for filename in files):
+        if not any(filename.startswith("caesarreflect") for filename in files):
             raise Error("File not found")
