121b7a8b3835ae649eaa16ef93817c117eecdd51
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-01-02 12:42:53 -0500

    Update __init__.py

--- pset2_reflection/check50/__init__.py
+++ pset2_reflection/check50/__init__.py
@@ -1,10 +1,10 @@
 from check50 import *
 
-class Techexplore(Checks):
+class Pset2(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Second Scratch Reflection'"""
+        """You submitted 'PSET2 Reflection'"""
         files = os.listdir()
-        if not any(filename.startswith("scratch_two_reflect") for filename in files):
+        if not any(filename.startswith("pset2_reflection") for filename in files):
             raise Error("File not found")
