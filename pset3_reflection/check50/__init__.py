fc13e4d3f6e665bdd58ba150393ee82d08629072
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-01-16 12:06:06 -0500

    Update __init__.py

--- pset3_reflection/check50/__init__.py
+++ pset3_reflection/check50/__init__.py
@@ -1,10 +1,10 @@
 from check50 import *
 
-class Pset2(Checks):
+class Pset3(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'PSET2 Reflection'"""
+        """You submitted 'PSET3 Reflection'"""
         files = os.listdir()
-        if not any(filename.startswith("pset2_reflection") for filename in files):
+        if not any(filename.startswith("pset3_reflection") for filename in files):
             raise Error("File not found")
