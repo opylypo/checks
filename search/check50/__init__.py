9ba6c15c618a50e4a7d8e73fbd86778b447d6129
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-02-08 08:57:19 -0500

    Update __init__.py

--- search/check50/__init__.py
+++ search/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Friendsreflect(Checks):
+class Search(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Friends Reflection'"""
+        """You submitted 'Search Engine'"""
         files = os.listdir()
-        if not any(filename.startswith("friendsreflect") for filename in files):
-            raise Error("File not found")
+#         if not any(filename.startswith("friendsreflect") for filename in files):
+#             raise Error("File not found")
