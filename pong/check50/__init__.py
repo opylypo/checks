f38268f02f5fa3769ec70de02f8d0b24e8f852e5
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-02-24 18:11:18 -0500

    Update __init__.py

--- pong/check50/__init__.py
+++ pong/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Marioreflect(Checks):
+class Pong(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Mario Reflection'"""
+        """You submitted 'Pong'"""
         files = os.listdir()
-        if not any(filename.startswith("marioreflect") for filename in files):
+        if not any(filename.startswith("pong") for filename in files):
             raise Error("File not found")
