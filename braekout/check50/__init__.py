ee3d62a0897a3b70462d2eeb9fde98b400a1d527
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-03-07 12:57:40 -0500

    Update __init__.py

--- braekout/check50/__init__.py
+++ braekout/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Pong(Checks):
+class Breakout(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Pong'"""
+        """You submitted 'Breakout'"""
         files = os.listdir()
-        if not any(filename.startswith("pong") for filename in files):
+        if not any(filename.startswith("breakout") for filename in files):
             raise Error("File not found")
