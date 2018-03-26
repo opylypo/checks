ffd424bc6572414367ae966d2e17d2339475f501
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-03-26 08:31:09 -0400

    Update __init__.py

--- arrays_reflect/check50/__init__.py
+++ arrays_reflect/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Caesarreflect(Checks):
+class Arrraysreflect(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Caesar Reflection'"""
+        """You submitted 'Arrays of Bubbles Reflection'"""
         files = os.listdir()
-        if not any(filename.startswith("caesarreflect") for filename in files):
+        if not any(filename.startswith("arrays_reflect") for filename in files):
             raise Error("File not found")
