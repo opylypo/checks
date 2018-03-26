84afda8c0ccff74b1a337f8d2df0f0db7597a8a3
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-03-26 08:30:14 -0400

    Update __init__.py

--- bubbles_reflect/check50/__init__.py
+++ bubbles_reflect/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Caesarreflect(Checks):
+class Bubblesreflect(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Caesar Reflection'"""
+        """You submitted 'Bubbles Reflection'"""
         files = os.listdir()
-        if not any(filename.startswith("caesarreflect") for filename in files):
+        if not any(filename.startswith("bubbles_reflect") for filename in files):
             raise Error("File not found")
