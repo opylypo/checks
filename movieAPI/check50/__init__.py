9210c22337d01e7436f17ef4e6dfdcf1e3c278ab
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-05-04 10:33:32 -0400

    Update __init__.py

--- movieAPI/check50/__init__.py
+++ movieAPI/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Temperature(Checks):
+class Movie(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Temperature'"""
+        """You submitted your 'Movie API Program'"""
         files = os.listdir()
-#         if not any(filename.startswith("temp") for filename in files):
+#         if not any(filename.startswith("movieAPI") for filename in files):
 #             raise Error("File not found")
