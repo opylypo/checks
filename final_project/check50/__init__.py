e4286a8be1404ea301bab58be0b8b70f3ab521ec
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-05-30 16:39:06 -0400

    Update __init__.py

--- final_project/check50/__init__.py
+++ final_project/check50/__init__.py
@@ -1,11 +1,11 @@
 from check50 import *
 import os
 
-class Pairprogram(Checks):
+class FinalProject(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Pair Programming Problem'"""
+        """You submitted 'Final Project Link'"""
         files = os.listdir()
-#         if not any(filename.startswith("skittles") for filename in files):
-#             raise Error("File not found")
+        if not any(filename.startswith("final_project") for filename in files):
+            raise Error("File not found")
