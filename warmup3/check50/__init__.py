6b2f88354e971f2d0221b6252e33bff8311680d5
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-10-09 09:00:03 -0400

    Update __init__.py

--- warmup3/check50/__init__.py
+++ warmup3/check50/__init__.py
@@ -14,22 +14,22 @@ class Sigma(Checks):
         self.spawn("clang -std=c11 -o calc sigma.c -lcs50 -lm").exit(0)
         
     @check("compiles")
-    def test_handles_1 argument(self):
+    def test_handles_1_argument(self):
         """sigma handles 10"""
         self.spawn("./sigma 10").stdout(number("10"), "10\n").exit(0)
 
     @check("compiles")
-    def test_handles_2 arguments(self):
+    def test_handles_2_arguments(self):
         """sigma handles 3 + 4"""
         self.spawn("./sigma 3 4").stdout(number("7"), "7\n").exit(0)
 
     @check("compiles")
-    def test_handles_4 arguments(self):
+    def test_handles_4_arguments(self):
         """sigma handles 1 + 2 + 3 + 4"""
         self.spawn("./sigma 1 2 3 4").stdout(number("10"), "10\n").exit(0)
 
     @check("compiles")
-    def test_handles_0 numbers(self):
+    def test_handles_0_arguments(self):
         """calculator handles no arguments"""
         self.spawn("./sigma").stdout(number(0), "0\n").exit(0)
 
