a76c37a66ffb184eeb3d18c59c27916231e553db
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-10-11 08:41:12 -0400

    Update __init__.py

--- linear/check50/__init__.py
+++ linear/check50/__init__.py
@@ -16,48 +16,15 @@ class Linear(Checks):
     @check("compiles")
     def test_handles_addition(self):
         """linear search finds Malan"""
-        self.spawn("./calc 3 + 4").stdout(number("7.000000"), "7.000000\n").exit(0)
+        self.spawn("./linear").stdin("Malan").stdout("Caling Malan\n").exit(0)
 
     @check("compiles")
     def test_handles_subtraction(self):
-        """calculator handles subtraction"""
-        self.spawn("./calc 10.5 - 6.2").stdout(number("4.300000"), "4.300000\n").exit(0)
+        """linear search finds Smith"""
+        self.spawn("./linear").stdin("Smith").stdout("Caling Smith\n").exit(0)
 
     @check("compiles")
     def test_handles_division(self):
-        """calculator handles division"""
-        self.spawn("./calc 41.48 / -8.44").stdout(number(-4.914692), "-4.914692\n").exit(0)
+        """linear search does not fine Tanzosh"""
+        self.spawn("./linear").stdin("Tanzosh").stdout("Quitting\n").exit(0)
 
-    @check("compiles")
-    def test_handles_multiplication_with_x(self):
-        """calculator handles multiplication with "x" """
-        self.spawn("./calc 11.1 x 9").stdout(number(99.900002),"99.900002\n").exit(0)
-
-    @check("compiles")
-    def test_handles_modulo(self):
-        """calculator handles modulo of integers"""
-        self.spawn("./calc 8 % 5").stdout(number("3.000000"), "3.000000\n").exit(0)
-
-    @check("compiles")
-    def test_handles_modulo2(self):
-        """calculator handles modulo of real numbers"""
-        self.spawn("./calc 8.1 % 4.9").stdout(number("3.200000"), "3.200000\n").exit(0)
-
-    @check("compiles")
-    def test_handles_bad_operation(self):
-        """handles invalid operation"""
-        self.spawn("./calc 11 J 8").exit(1)
-
-    @check("compiles")
-    def test_argc_1(self):
-        """handles lack of command line arguments"""
-        self.spawn("./calc").exit(1)
-
-    @check("compiles")
-    def test_argc_5(self):
-        """handles too many command line arguments"""
-        self.spawn("./calc 11.1 + 23 9").exit(1)
-
-
-def number(num):
-    return "(^|[^\d]){}[^\d]".format(num)
