ca69118680bec5f0168317ca52d8e18dc35ee8ed
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-10-22 06:35:08 -0400

    Update __init__.py

--- insert/check50/__init__.py
+++ insert/check50/__init__.py
@@ -1,44 +1,37 @@
 from check50 import *
 
 
-class Glocal(Checks):
+class Insert(Checks):
 
     @check()
     def exists(self):
-        """glocal.c exists"""
+        """insert.c exists"""
         self.require("glocal.c")
 
     @check("exists")
     def compiles(self):
-        """glocal.c compiles"""
-        self.spawn("clang -std=c11 -o glocal glocal.c -lcs50 -lm").exit(0)
+        """insert.c compiles"""
+        self.spawn("clang -std=c11 -o insert insert.c -lcs50 -lm").exit(0)
 
     @check("compiles")
     def test1(self):
-        """input of 1 yields output of 6"""
-        self.spawn("./glocal").stdin("1").stdout(number(6), "6\n").exit(0)
+        """input of 10.2 yields output of -3.5 5.6 7.0 8.2 9.7 10.2 18.3"""
+        self.spawn("./glocal").stdin("10.2").stdout("-3.5 5.6 7.0 8.2 9.7 10.2 18.3 \n").exit(0)
 
     @check("compiles")
     def test0(self):
-        """input of 0 yields output of 5"""
-        self.spawn("./glocal").stdin("0").stdout(number(5), "5\n").exit(0)
+        """input of 23.1 yields output of -3.5 5.6 7.0 8.2 9.7 18.3 23.1"""
+        self.spawn("./glocal").stdin("23.1").stdout("-3.5 5.6 7.0 8.2 9.7 18.3 23.1 \n").exit(0)
 
     @check("compiles")
     def test100(self):
-        """input of 100 yields output of 105"""
-        self.spawn("./glocal").stdin("100").stdout(number(105), "105\n").exit(0)
+        """input of 0 yields output of -3.5 0.0 5.6 7.0 8.2 9.7 18.3"""
+        self.spawn("./glocal").stdin("0").stdout("-3.5 0.0 5.6 7.0 8.2 9.7 18.3 \n").exit(0)
+           
+   @check("compiles")
+    def test100(self):
+        """input of -5.8 yields output of -5.8 -3.5 5.6 7.0 8.2 9.7 18.3"""
+        self.spawn("./glocal").stdin("-5.8").stdout("-5.8 -3.5 5.6 7.0 8.2 9.7 18.3 \n").exit(0)
 
-    @check("compiles")
-    def test_reject_foo(self):
-        """rejects a non-numeric input of "foo" """
-        self.spawn("./glocal").stdin("foo").reject()
+    
 
-    @check("compiles")
-    def test_reject_empty_string(self):
-        """rejects a non-numeric input of "" """
-        self.spawn("./glocal").stdin("").reject()
-
-
-
-def number(num):
-    return "(^|[^\d]){}[^\d]".format(num)
