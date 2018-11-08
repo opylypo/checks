594bbfc17a2e0bb68fe76982dfc76aa965711221
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-11-08 11:53:54 -0500

    Update __init__.py

--- cube/check50/__init__.py
+++ cube/check50/__init__.py
@@ -1,38 +1,43 @@
+import re
+
 from check50 import *
 
-
-class Sigma(Checks):
+class Cube(Checks):
 
     @check()
     def exists(self):
-        """sigma.c exists"""
-        self.require("sigma.c")
+        """cube.py exists"""
+        self.require("cube.py")
 
     @check("exists")
-    def compiles(self):
-        """sigma.c compiles"""
-        self.spawn("clang -std=c11 -o sigma sigma.c -lcs50 -lm").exit(0)
-        
-    @check("compiles")
-    def test_handles_1_argument(self):
-        """sigma handles 10"""
-        self.spawn("./sigma 10").stdout(number("10"), "10\n").exit(0)
+    def test041(self):
+        """input of 1 yields output of 1"""
+        self.spawn("python3 cube.py").stdin("1").stdout("1\n").exit(0)
 
-    @check("compiles")
-    def test_handles_2_arguments(self):
-        """sigma handles 3 + 4"""
-        self.spawn("./sigma 3 4").stdout(number("7"), "7\n").exit(0)
+    @check("exists")
+    def test001(self):
+        """input of 2 yields output of 8"""
+        self.spawn("python3 cube.py").stdin("2").stdout("8\n").exit(0)
 
-    @check("compiles")
-    def test_handles_4_arguments(self):
-        """sigma handles 1 + 2 + 3 + 4"""
-        self.spawn("./sigma 1 2 3 4").stdout(number("10"), "10\n").exit(0)
-
-    @check("compiles")
-    def test_handles_0_arguments(self):
-        """calculator handles no arguments"""
-        self.spawn("./sigma").stdout(number(0), "0\n").exit(0)
+    @check("exists")
+    def test015(self):
+        """input of 10 yields output of 1000"""
+        self.spawn("python3 cube.py").stdin("10").stdout("1000\n").exit(0)
 
 
-def number(num):
-    return "(^|[^\d]){}[^\d]".format(num)
+    @check("exists")
+    def test_reject_negative(self):
+        """rejects a non-integer input like 1.2"""
+        self.spawn("python3 cube.py").stdin("1.2").reject()
+
+    @check("exists")
+    def test_reject_foo(self):
+        """rejects a non-numeric input of "foo" """
+        self.spawn("python3 cube.py").stdin("foo").reject()
+
+    @check("exists")
+    def test_reject_empty(self):
+        """rejects a non-numeric input of "" """
+        self.spawn("python3 cube.py").stdin("").reject()
+
+
