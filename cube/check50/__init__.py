653fd134eff7c37390bfa4dc77ecb926fd152939
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2018-11-08 11:48:33 -0500

    Add files via upload

---
+++ cube/check50/__init__.py
@@ -0,0 +1,38 @@
+from check50 import *
+
+
+class Sigma(Checks):
+
+    @check()
+    def exists(self):
+        """sigma.c exists"""
+        self.require("sigma.c")
+
+    @check("exists")
+    def compiles(self):
+        """sigma.c compiles"""
+        self.spawn("clang -std=c11 -o sigma sigma.c -lcs50 -lm").exit(0)
+        
+    @check("compiles")
+    def test_handles_1_argument(self):
+        """sigma handles 10"""
+        self.spawn("./sigma 10").stdout(number("10"), "10\n").exit(0)
+
+    @check("compiles")
+    def test_handles_2_arguments(self):
+        """sigma handles 3 + 4"""
+        self.spawn("./sigma 3 4").stdout(number("7"), "7\n").exit(0)
+
+    @check("compiles")
+    def test_handles_4_arguments(self):
+        """sigma handles 1 + 2 + 3 + 4"""
+        self.spawn("./sigma 1 2 3 4").stdout(number("10"), "10\n").exit(0)
+
+    @check("compiles")
+    def test_handles_0_arguments(self):
+        """calculator handles no arguments"""
+        self.spawn("./sigma").stdout(number(0), "0\n").exit(0)
+
+
+def number(num):
+    return "(^|[^\d]){}[^\d]".format(num)
