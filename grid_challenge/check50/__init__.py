c1e958a4750e4471a09d5553c8d63ae71a6e8378
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-03-11 08:34:44 -0400

    Update __init__.py

--- grid_challenge/check50/__init__.py
+++ grid_challenge/check50/__init__.py
@@ -1,11 +1,10 @@
 from check50 import *
 import os
 
-class Pairprogram(Checks):
+class Gridchallenge(Checks):
 
     @check()
     def submitted(self):
-        """You submitted 'Pair Programming Problem'"""
+        """You submitted 'Grid Challenge'"""
         files = os.listdir()
-#         if not any(filename.startswith("skittles") for filename in files):
-#             raise Error("File not found")
+
