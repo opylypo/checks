568bfdc13355e0b212f7fbae638c602ace515aec
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-04-10 15:44:45 -0400

    Update __init__.py

--- fahrenheit-python/check50/__init__.py
+++ fahrenheit-python/check50/__init__.py
@@ -15,20 +15,20 @@ class Fahrenheit(Checks):
         """37 degrees Celsius yields 98.6 degrees Fahrenheit"""
         self.spawn("python3 fahrenheit.py").stdin("37").stdout(number(98.6), "98.6\n").exit(0)
 
-#     @check("exists")
-#     def test0(self):
-#        """0 degrees Celsius yields 32.0 degrees Fahrenheit"""
-#         self.spawn("python3 fahrenheit.py").stdin("0").stdout(number(32.0), "32.0\n").exit(0)
+    @check("exists")
+    def test0(self):
+       """0 degrees Celsius yields 32.0 degrees Fahrenheit"""
+        self.spawn("python3 fahrenheit.py").stdin("0").stdout(number(32.0), "32.0\n").exit(0)
 
-#     @check("exists")
-#     def test100(self):
-#         """100.00 degrees Celsius yields 212.0 degrees Fahrenheit"""
-#         self.spawn("python3 fahrenheit.py").stdin("100.00").stdout(number(212.0), "212.0\n").exit(0)
+    @check("exists")
+    def test100(self):
+        """100.00 degrees Celsius yields 212.0 degrees Fahrenheit"""
+        self.spawn("python3 fahrenheit.py").stdin("100.00").stdout(number(212.0), "212.0\n").exit(0)
            
-#     @check("exists")
-#     def test18point5(self):
-#         """18.5 degrees Celsius yields 65.3 degrees Fahrenheit"""
-#         self.spawn("python3 fahrenheit.py").stdin("18.5").stdout(number(65.3), "65.3\n").exit(0)
+    @check("exists")
+    def test18point5(self):
+        """18.5 degrees Celsius yields 65.3 degrees Fahrenheit"""
+        self.spawn("python3 fahrenheit.py").stdin("18.5").stdout(number(65.3), "65.3\n").exit(0)
         
 #     @check("exists")
 #     def testneg123point45678(self):
