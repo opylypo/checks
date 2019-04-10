c2b1eaf209687163d0d19ef105d870904bcbacca
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-04-10 13:07:03 -0400

    Update __init__.py

--- fahrenheit-python/check50/__init__.py
+++ fahrenheit-python/check50/__init__.py
@@ -1,30 +1,29 @@
 from check50 import *
 
 
-class Bin_search(Checks):
+cclass Fahrenheit(Checks):
 
     @check()
     def exists(self):
-        """bin_search.c exists"""
-        self.require("bin_search.c")
+        """fahrenheit.py exists"""
+        self.require("fahrenheit.py")
 
     @check("exists")
-    def compiles(self):
-        """bin_search.c compiles"""
-        self.spawn("clang -std=c11 -o bin_search bin_search.c -lcs50 -lm").exit(0)
+    def test37(self):
+        """37 degrees Celsius yields 98.6 degrees Fahrenheit"""
+        self.spawn("python3 fahrenheit.py").stdin("37").stdout(number(98.6), "98.6\n").exit(0)
 
-    @check("compiles")
-    def test_handles_addition(self):
-        """linear search finds 2"""
-        self.spawn("./bin_search").stdin("2").stdout("Found\n").exit(0)
+    @check("exists")
+    def test0(self):
+       """0 degrees Celsius yields 32.0 degrees Fahrenheit"""
+        self.spawn("python3 fahrenheit.py").stdin("0").stdout(number(32.0), "32.0\n").exit(0)
 
-    @check("compiles")
-    def test_handles_subtraction(self):
-        """linear search finds 14"""
-        self.spawn("./bin_search").stdin("14").stdout("Found\n").exit(0)
-
-    @check("compiles")
-    def test_handles_division(self):
-        """linear search does not fine 9"""
-        self.spawn("./bin_search").stdin("9").stdout("Not found!\n").exit(0)
-
+    @check("exists")
+    def test100(self):
+        """100.00 degrees Celsius yields 212.0 degrees Fahrenheit"""
+        self.spawn("python3 fahrenheit.py").stdin("100.00").stdout(number(212.0), "212.0\n").exit(0)
+           
+    @check("exists")
+    def test100(self):
+        """18.5 degrees Celsius yields 65.3 degrees Fahrenheit"""
+        self.spawn("python3 fahrenheit.py").stdin("18.5").stdout(number(65.3), "65.3\n").exit(0)
