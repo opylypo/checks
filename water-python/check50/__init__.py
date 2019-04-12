4fd7a7d9d976dffa6ca4bec7317bb2cb45559d73
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2019-04-11 20:01:06 -0400

    Update __init__.py

--- water-python/check50/__init__.py
+++ water-python/check50/__init__.py
@@ -3,49 +3,52 @@ import re
 from check50 import *
 
 
-class Fahrenheit(Checks):
+class Water(Checks):
 
     @check()
     def exists(self):
-        """fahrenheit.py exists"""
-        self.require("fahrenheit.py")
+        """water.py exists"""
+        self.require("water.py")
 
     @check("exists")
-    def test37(self):
-        """37 degrees Celsius yields 98.6 degrees Fahrenheit"""
-        self.spawn("python3 fahrenheit.py").stdin("37").stdout(number(98.6), "98.6\n").exit(0)
-
-    @check("exists")
-    def test0(self):
-        """0 degrees Celsius yields 32.0 degrees Fahrenheit"""
-        self.spawn("python3 fahrenheit.py").stdin("0").stdout(number(32.0), "32.0\n").exit(0)
-
-    @check("exists")
-    def test100(self):
-        """100.00 degrees Celsius yields 212.0 degrees Fahrenheit"""
-        self.spawn("python3 fahrenheit.py").stdin("100.00").stdout(number(212.0), "212.0\n").exit(0)
-           
-    @check("exists")
-    def test18point5(self):
-        """18.5 degrees Celsius yields 65.3 degrees Fahrenheit"""
-        self.spawn("python3 fahrenheit.py").stdin("18.5").stdout(number(65.3), "65.3\n").exit(0)
+    def test1(self):
+        """1 minute equals 12 bottles."""
+        self.spawn("python water.py").stdin("1").stdout(bottles(12), "12\n")
         
     @check("exists")
-    def testneg123point45678(self):
-        """-123.45678 degrees Celsius yields -190.2 degrees Fahrenheit"""
-        self.spawn("python3 fahrenheit.py").stdin("-123.45678").stdout(number(-190.2), "-190.2\n").exit(0)
+    def test2(self):
+        """2 minute equals 24 bottles."""
+        self.spawn("python water.py").stdin("2").stdout(bottles(24), "24\n")
+        
+    @check("exists")
+    def test5(self):
+        """5 minute equals 60 bottles."""
+        self.spawn("python water.py").stdin("5").stdout(bottles(60), "60\n").exit(0)
+        
+    @check("exists")
+    def test10(self):
+        """10 minute equals 120 bottles."""
+        self.spawn("python water.py").stdin("10").stdout(bottles(120), "120\n").exit(0)    
 
     @check("exists")
     def test_reject_foo(self):
         """rejects a non-numeric input of "foo" """
-        self.spawn("python3 fahrenheit.py").stdin("foo").reject()
+        self.spawn("python3 water.py").stdin("foo").reject()
         
     @check("exists")
-    def test_reject_empty_string(self):
-        """rejects a non-numeric input of "" """
-        self.spawn("python3 fahrenheit.py").stdin("").reject()
+    def test_reject_empty(self):
+        """rejects "" minutes"""
+        self.spawn("python3 water.py").stdin("").reject()
+        
+    @check("exists")
+    def test_reject_123abc(self):
+        """rejects "123abc" minutes"""
+        self.spawn("python water").stdin("123abc").reject()
 
 
+        
+        
 
-def number(num):
-    return "(^|[^\d]){}[^\d]".format(num)    
+
+def bottles(num):
+    return "(^|[^\d]){}[^\d]".format(num)
