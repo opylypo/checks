f3ca2147cbf4b4dbc0d24dfaf58e85c360797ff6
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-01-07 08:15:26 -0500

    Update __init__.py

--- pw_checker/__init__.py
+++ pw_checker/__init__.py
@@ -9,28 +9,26 @@ def exists():
 
 @check50.check(exists)
 def compiles():
-    """initials.c compiles."""
-    check50.c.compile("initials.c", lcs50=True)
+    """pwcheck.c compiles."""
+    check50.c.compile("pwcheck.c", lcs50=True)
 
 @check50.check(compiles)
-def uppercase():
-    """Outputs HLJ for Hailey Lynn James"""
-    check50.run("./initials").stdin("Hailey Lynn James", prompt=False).stdout(match("HLJ"), "HLJ\n").exit(0)
+def lowercaseonly():
+    """Outputs Does Not Pass"""
+    check50.run("./pwcheck").stdin("abcdefgh", prompt=False).stdout("Does Not Pass\n").exit(0)
 
 @check50.check(compiles)
-def lowercase():
-    """Outputs HLJ for hailey lynn james"""
-    check50.run("./initials").stdin("hailey lynn james", prompt=False).stdout(match("HLJ"), "HLJ\n").exit(0)
+def uppercaseonly():
+    """Outputs Does Not Pass"""
+    check50.run("./pwcheck").stdin("ABCDEFGH", prompt=False).stdout("Does Not Pass\n").exit(0)
 
 @check50.check(compiles)
 def mixed_case():
-    """Outputs HJ for hailey James"""
-    check50.run("./initials").stdin("hailey James", prompt=False).stdout(match("HJ"), "HJ\n").exit(0)
+    """Outputs Does Not Pass"""
+    check50.run("./pwcheck").stdin("AbCcDd12", prompt=False).stdout("Does Not Pass\n").exit(0)
 
 @check50.check(compiles)
 def all_uppercase():
-    """Outputs B for BRIAN"""
-    check50.run("./initials").stdin("BRIAN", prompt=False).stdout(match("B"), "B\n").exit(0)
+    """Outputs Passes"""
+    check50.run("./pwcheck").stdin("AbCcDd1!", prompt=False).stdout("Passes\n").exit(0)
 
-def match(initials):
-    return "^(.*\n)?{}\n".format(initials)
