ca2be346f9100decba2a706c941f6118662b1eae
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-01-19 10:06:54 -0500

    Update __init__.py

--- hellojs/__init__.py
+++ hellojs/__init__.py
@@ -11,15 +11,15 @@ def exists():
     """hello.js exists."""
     check50.exists("hello.js")
 
-@check50.check(exists)
-def prints_hello():
-    """prints "hello, MT\\n" """
-    from re import match
+# @check50.check(exists)
+# def prints_hello():
+#     """prints "hello, MT\\n" """
+#     from re import match
 
-    expected = "[Hh]ello,MT\n"
-    actual = check50.run('node -e \'require("./hello").hello("MT")\'').stdout()
-    if not match(expected, actual):
-        help = None
-        if match(expected[:-1], actual):
-            help = r"did you forget a newline ('\n') at the end of your printf string?"
-        raise check50.Mismatch("hello, MT\n", actual, help=help)
+#     expected = "[Hh]ello,MT\n"
+#     actual = check50.run('node -e \'require("./hello").hello("MT")\'').stdout()
+#     if not match(expected, actual):
+#         help = None
+#         if match(expected[:-1], actual):
+#             help = r"did you forget a newline ('\n') at the end of your printf string?"
+#         raise check50.Mismatch("hello, MT\n", actual, help=help)
