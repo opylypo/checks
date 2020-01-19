3de7f86edf0425a4a2a2b378389f9cc49d7a8a58
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-01-19 09:53:01 -0500

    Update __init__.py

--- hellojs/__init__.py
+++ hellojs/__init__.py
@@ -2,6 +2,12 @@ import check50
 import check50.c
 import re
 
+f = open("hello.js", 'b')
+f.write("module.exports = {
+    hello,
+}")
+f.close()
+
 @check50.check()
 def exists():
     """hello.js exists."""
@@ -9,13 +15,13 @@ def exists():
 
 @check50.check(exists)
 def prints_hello():
-    """prints "hello, world\\n" """
+    """prints "hello, MT\\n" """
     from re import match
 
-    expected = "[Hh]ello, world!?\n"
-    actual = check50.run("node hello.js").stdout()
+    expected = "[Hh]ello,MT\n"
+    actual = check50.run("node -e 'require("./hello").hello("MT")'").stdout()
     if not match(expected, actual):
         help = None
         if match(expected[:-1], actual):
             help = r"did you forget a newline ('\n') at the end of your printf string?"
-        raise check50.Mismatch("hello, world\n", actual, help=help)
+        raise check50.Mismatch("hello, MT\n", actual, help=help)
