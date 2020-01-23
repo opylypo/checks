e171de6de6ea45432a7fdf90e7c31ecdaad3dc4e
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-01-22 20:42:52 -0500

    Add files via upload

---
+++ js/foo/__init__.py
@@ -0,0 +1,17 @@
+import check50
+import bond
+
+@check50.check()
+def foo_exists():
+  """ Function foo exists """
+
+  # Create a JavaScript interface
+  i = bond.make_bond("JavaScript")
+  # Read foo.js
+  with open("foo.js") as f:
+    # Evaluate the code in foo.js with node
+    i.eval_block(f.read())
+
+    # Check if foo is a function
+    if i.eval("typeof(foo) !== 'function'"):
+      raise check50.Failure("foo is not defined")
