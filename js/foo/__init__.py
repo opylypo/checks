f92ec42f73c7e568338a5519f36868303d5ae5d2
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-01-22 20:44:05 -0500

    Update __init__.py

--- js/foo/__init__.py
+++ js/foo/__init__.py
@@ -5,13 +5,13 @@ import bond
 def foo_exists():
   """ Function foo exists """
 
-  # Create a JavaScript interface
-  i = bond.make_bond("JavaScript")
-  # Read foo.js
-  with open("foo.js") as f:
-    # Evaluate the code in foo.js with node
-    i.eval_block(f.read())
+    # Create a JavaScript interface
+    i = bond.make_bond("JavaScript")
+    # Read foo.js
+    with open("foo.js") as f:
+        # Evaluate the code in foo.js with node
+        i.eval_block(f.read())
 
-    # Check if foo is a function
-    if i.eval("typeof(foo) !== 'function'"):
-      raise check50.Failure("foo is not defined")
+        # Check if foo is a function
+        if i.eval("typeof(foo) !== 'function'"):
+            raise check50.Failure("foo is not defined")
