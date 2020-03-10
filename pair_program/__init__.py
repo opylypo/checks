e2c8128bc9d40c825bba3bb7b46329e1e08e8542
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-03-10 15:31:23 -0400

    Update __init__.py

--- pair_program/__init__.py
+++ pair_program/__init__.py
@@ -1,9 +1,5 @@
 import check50
-import check50.c
-import re
-
 
 @check50.check()
-def exists(self):
-    """tempreflect.txt exists."""
-    check50.exists("tempreflect.txt")
+def submitted():
+    """Pair Programming Problem submitted"""
