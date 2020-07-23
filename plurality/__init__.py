cebf3495a3d3bcf360d7917acb43bea245a3d6f9
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-07-23 13:16:49 -0400

    Update __init__.py

--- plurality/__init__.py
+++ plurality/__init__.py
@@ -22,14 +22,14 @@ def prepare():
 @check50.hidden("vote function did not return True")
 def vote_finds_name():
     """vote returns true when given name of candidate"""
-    check50.run("python3 plurality_test.py 0").stdout("True").exit(0)
+    check50.run("python3 testing.py 0").stdout("True").exit(0)
   
 
 @check50.check(compiles)
 @check50.hidden("vote function did not return False")
 def vote_returns_false():
     """vote returns false when given name of invalid candidate"""
-    check50.run("python3 plurality_test.py 1").stdout("False").exit(0)
+    check50.run("python3 testing.py 1").stdout("False").exit(0)
   
 
 @check50.check(exists)
