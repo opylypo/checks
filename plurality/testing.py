a4abf25075ca0b92a2e6b0204fd942b13048bf15
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-07-23 13:13:14 -0400

    Update testing.py

--- plurality/testing.py
+++ plurality/testing.py
@@ -1,5 +1,32 @@
-from plurality import vote, print_winner
+from plurality_test import vote, print_winner
 from sys import argv
+
+# Check for invalid usage
+if len(argv) != 2:
+    exit(0)
+
+test = int(argv[1])
+
+candidate_count = 3
+candidates = {}
+
+candidates["Alice"] = 0
+candidates["Bob"] = 0
+candidates["Charlie"] = 0
+
+def case0():
+    print(vote("Alice"))
+
+def case1():
+    print(vote("David"))
+
+# map the inputs to the function blocks
+options = {0 : case0,
+           1 : case1
+}
+
+options[test]()
+
 
 
 
