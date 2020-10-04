a2e277f7e2f9586e9fba370bb883a056daf131f6
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-10-04 16:51:54 -0400

    Update runoff_test.py

--- runoff/runoff_test.py
+++ runoff/runoff_test.py
@@ -1,6 +1,6 @@
 import sys
 
-from plurality import vote, Candidate
+from plurality import vote, tabulate, print_winner, find_min, is_tie, Candidate
 from dataclasses import dataclass
 
 @dataclass
@@ -18,19 +18,19 @@ def main():
     global preferences
 
     # Determine which test to run
-    setup = int(argv[1]);
-    test = int(argv[2]);   
+    setup = int(argv[1])
+    test = int(argv[2])   
                    
     if setup == 0:
-        voter_count = 5;
-        candidate_count = 3;
+        voter_count = 5
+        candidate_count = 3
         candidates.append(Candidate('Alice', 0, False))
         candidates.append(Candidate('Bob', 0, False))
         candidates.append(Candidate('Charlie', 0, False))
         
     elif setup == 1:
-        voter_count = 7;
-        candidate_count = 4;
+        voter_count = 7
+        candidate_count = 4
         candidates.append(Candidate('Alice', 0, False))
         candidates.append(Candidate('Bob', 0, False))
         candidates.append(Candidate('Charlie', 0, False))
@@ -63,53 +63,61 @@ def main():
         preferences.append(Preference(6, 1, 2))
         preferences.append(Preference(6, 2, 1))
         preferences.append(Preference(6, 3, 3))
-       
-  
-  
         
+    elif setup == 2:
+        voter_count = 28
+        candidate_count = 4
+        candidates.append(Candidate('Alice', 8, False))
+        candidates.append(Candidate('Bob', 15, False))
+        candidates.append(Candidate('Charlie', 4, False))
+        candidates.append(Candidate('David', 1, False))
+            
     if test == 0:
         print(vote(0, 0, "Bob"))
         
-     elif test == 1:
+    elif test == 1:
         print(vote(1, 2, "David"))
             
-     elif test == 2:
+    elif test == 2:
         vote(0, 0, "Charlie")
         print(len(preferences) == 1 and preferences[0] == Preference(0, 0, 2))
         
-     elif test == 3:
+    elif test == 3:
         vote(1, 2, "Alice")
         print(len(preferences) == 1 and preferences[0] == Preference(1, 2, 0))
         
-     elif test == 4:
+    elif test == 4:
         vote(1, 0, "Bob")
         vote(1, 1, "Alice")
         vote(1, 2, "Charlie")
         print(len(preferences) == 3 and preferences[0] = Preference(1, 0, 2) and preferences[1] == Preference(1, 1, 0) and preferences[2] = Preference(1, 2, 2))
         
-              
-              
+    elif test == 5:
+        tabulate()
+        for candidate in candidates:
+            print(candidate.votes)
+            
+    elif test == 6:     
+        candidates[3].eliminated = True
+        tabulate()
+        for candidate in candidates:
+            print(candidate.votes)
+            
+    elif test == 7:
+        candidates[2].eliminated = True
+        candidates[3].eliminated = True
+        tabulate()
+        for candidate in candidates:
+            print(candidate.votes)
+            
+    elif test == 8:
+        print_winner()
+        
+    elif test == 9:
+        
+        
         
             
-    if any(x.name == candidate for x in candidates):
-        # vote returns True when candidate exists
-        assert vote(0, 0, candidate)
-
-        # vote sets preference
-        assert (preferences[0] == Preference(0, 0, 0))
-
-        assert vote(0, 0, candidate2)
-        assert (assert(len(preferences) == 2 and preferences[1] == Preference(0, 0, 0))
-
-        # vote doesn't change any other candidates' votes
-        assert all(candidates[c] == candidates_[c] for c in candidates_ if c != candidate)
-    else:
-        # vote returns False when candidate doesn't exist
-        assert not vote(0, 0, candidate)
-
-        # vote count stays the same
-        assert candidates == candidates_
-
-
+    
 if __name__ == "__main__":
     main()
