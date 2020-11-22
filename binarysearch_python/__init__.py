e12449e0a5f9acd8a2ae3fec099012351c1eb329
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-11-22 18:04:17 -0500

    Update __init__.py

--- binarysearch_python/__init__.py
+++ binarysearch_python/__init__.py
@@ -10,17 +10,17 @@ def exists():
 @check50.check(exists)
 def finds2():
     """binary search finds 2"""
-    check50.run("python3 binary.py").stdin("2").stdout("Found\n").exit(0)
+    check50.run("python3 binary.py").stdin("2").stdout("Found!\n").exit(0)
     
 
 @check50.check(exists)
 def finds14():
     """binary search finds 14"""
-    check50.run("python3 binary.py").stdin("14").stdout("Found\n").exit(0)   
+    check50.run("python3 binary.py").stdin("14").stdout("Found!\n").exit(0)   
     
     
 @check50.check(exists)
 def finds14():
     """binary search does not find 9"""
-    check50.run("python3 binary.py").stdin("9").stdout("Not Found\n").exit(0)        
+    check50.run("python3 binary.py").stdin("9").stdout("Not found!\n").exit(0)        
     
