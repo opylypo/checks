e0706feef9625aca9a28b5963cff0c4fe8afb500
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-11-22 18:16:19 -0500

    Create __init__.py

---
+++ linearsearch_python/__init__.py
@@ -0,0 +1,25 @@
+import check50
+import re
+
+@check50.check()
+def exists():
+    """linear.py exists"""
+    check50.exists("linear.py")
+    
+    
+@check50.check(exists)
+def finds2():
+    """linear search finds 7"""
+    check50.run("python3 linear.py").stdin("7").stdout("Bingo! Found 7!\n").exit(0)
+    
+
+@check50.check(exists)
+def finds14():
+    """binary search finds 26"""
+    check50.run("python3 linear.py").stdin("26").stdout("Bingo! Found 26!\n").exit(0)   
+    
+    
+@check50.check(exists)
+def finds14():
+    """binary search does not find 70"""
+    check50.run("python3 linear.py").stdin("70").stdout("Sorry better luck next time!\n").exit(0)   
