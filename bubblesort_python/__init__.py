154a5956aa9cd8049178aa47415bd93e776bb867
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-11-22 18:59:06 -0500

    Update __init__.py

--- bubblesort_python/__init__.py
+++ bubblesort_python/__init__.py
@@ -9,5 +9,5 @@ def exists():
     
 @check50.check(exists)
 def sorts():
-    """sorts {64, 34, 25, 12, 22, 11, 90}"""
-    check50.run("python3 bubble.py").stdout("11 12 22 25 34 64 90 \n").exit(0)
+    """sorts [1, 8, 4, 6, 0, 3, 5, 2, 7, 9]"""
+    check50.run("python3 bubble.py").stdout("0 1 2 3 4 6 7 8 9 \n").exit(0)
