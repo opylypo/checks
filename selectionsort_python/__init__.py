43efbbd06910a1ae0cf641bec45892262711dd77
Margaret NestM <31802801+cs50nestm@users.noreply.github.com>
2020-11-22 19:14:08 -0500

    Create __init__.py

---
+++ selectionsort_python/__init__.py
@@ -0,0 +1,13 @@
+import check50
+import re
+
+@check50.check()
+def exists():
+    """selection.py exists"""
+    check50.exists("selection.py")
+    
+    
+@check50.check(exists)
+def sorts():
+    """sorts [1, 8, 4, 6, 0, 3, 5, 2, 7, 9]"""
+    check50.run("python3 selection.py").stdout("0 1 2 3 4 5 6 7 8 9 \n").exit(0)
